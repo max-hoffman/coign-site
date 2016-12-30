0e6e3123ff00e439fc96e7b4a8e95bf775bf1c58
Max Hoffman <maximilian.wolfgang1@gmail.com>
2016-12-30 10:54:43 -0600

    Posts page now loads with variable args passed from app.py

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from firebase import firebase
 
 
 application = Flask(__name__)
 Bootstrap(application)
+
 
 @application.route("/")
 def index():
@@ -16,6 +18,19 @@ def mission():
 @application.route("/contact")
 def contact():
 	return render_template("contact.html")
+
+@application.route("/posts/<path>")
+def post(path):
+	postData = getPost(path)
+	return render_template("post.html", data = postData)
+
+#fb = firebase.firebaseApplication("https://coign-dev.firebaseio.com/", None)
+##firebase data retrieval
+def getPost(postID):
+	#result = firebase.get('/users', postID)
+	result = postID
+	print(result) 
+	return result
 
 if __name__ == "__main__":
 	application.run(debug = True)
