ee00e0af1f21faa8fccc41be20d34adc42f3dd99
Max Hoffman <maximilian.wolfgang1@gmail.com>
2016-12-31 15:37:59 -0600

    Adds the terms and condition page, skeleton of the private policy page

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from firebase import firebase
+from twilio.rest import TwilioRestClient
 
 
 application = Flask(__name__)
@@ -19,11 +20,33 @@ def mission():
 def contact():
 	return render_template("contact.html")
 
+@application.route("/terms")
+def terms():
+	return render_template("terms.html")
+
+@application.route("/ppolicy")
+def ppolicy():
+	return render_template("ppolicy.html")
+
 @application.route("/posts/<path>")
 def post(path):
 	postData = getPost(path)
 	return render_template("post.html", data = postData)
 
+# @application.route('/text', methods=['POST'])
+# def text():
+# 	#get the phone number
+# 	phone_number=request.form.get('phoneNumber', None)
+
+# 	# Find these values at https://twilio.com/user/account
+# 	account_sid = "AC64bff1796402535465c9bebe442d1f18"
+# 	auth_token = "73e624975dec2ef220940fea6467ce9e"
+                                     
+# 	try:
+# 		# twilio_client=TwilioRestClient(app.config['TWILIO_ACCOUNT_SID'],app.config['TWILIO_AUTH_TOKEN'])
+# 		twilio_client = TwilioRestClient(account_sid, auth_token)
+# 		message = client.messages.create(to=phone_number, from_="+15555555555",
+#                                      body="Download Coign at www....")
 
 ##firebase data retrieval
 def getPost(postID):
